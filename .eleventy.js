a3d35fcbace7e6253d3630c4d4a5ccc08d4c0931
dithidi <brent.jlinville@gmail.com>
2022-08-15 09:56:18 -0500

    add meta data; finish game map entry

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("dist/css");
+    eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
+    eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
     return {
         dir: {
